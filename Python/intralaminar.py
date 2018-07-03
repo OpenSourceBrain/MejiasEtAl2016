028d30eb8b191aa36552b651f1ef832fa911a5a8
Jessica Dafflon <jejedafflon@gmail.com>
2018-07-03 13:12:53 +0100

    Python: Pass interalaminar parameters as arguments to the script
    * Pass tau_e, tau_i and sigma_e as arguments to the script

--- Python/intralaminar.py
+++ Python/intralaminar.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pylab as plt
-
+import argparse
 
 def transduction_function(x):
     # note: define boundary conditions for the transduction function
@@ -13,21 +13,25 @@ def transduction_function(x):
         return x / (1 - math.exp(-x))
 
 
-def calculate_firing_rate(dt, re, ri, wee, wie, wei, wii, tau_e, tau_i):
-    dE = dt * (-re + transduction_function((wee * re) + (wei * ri)) + math.sqrt(tau_e) * np.random.normal(0, sigma))/tau_e
-    dI = dt * (-ri + transduction_function((wie * re) + (wii * ri)) + math.sqrt(tau_i) * np.random.normal(0, sigma))/tau_i
+def calculate_firing_rate(dt, re, ri, wee, wie, wei, wii, tau_e, tau_i, sei):
+    dE = dt * (-re + transduction_function((wee * re) + (wei * ri)) + math.sqrt(tau_e) * np.random.normal(0, sei))/tau_e
+    dI = dt * (-ri + transduction_function((wie * re) + (wii * ri)) + math.sqrt(tau_i) * np.random.normal(0, sei))/tau_i
     uu_p = re + dE
     vv_p = ri + dI
     return uu_p, vv_p
+
+parser = argparse.ArgumentParser(description='Parameters for the simulation')
+parser.add_argument('-tau_e', type=float, dest='tau_e', help='Excitatory membrane time constant (tau_e)')
+parser.add_argument('-tau_i', type=float, dest='tau_i', help='Inhibitory membrane time constant (tau_i)')
+parser.add_argument('-sei', type=float, dest='sei', help='Deviation for the Gaussian white noise (s_ei)')
+
+args = parser.parse_args()
 
 ## superficial layer
 wee = 1.5
 wei = -3.25
 wie = 3.5
 wii = -2.5
-tau_e = 6
-tau_i = 15
-sigma = .3
 
 dt = .05
 tstop = 100
@@ -39,14 +43,20 @@ uu_p[0] = 0.0001
 vv_p[0] = 0.0001
 
 for dt_idx in range(len(t)):
-    uu_p[dt_idx + 1], vv_p[dt_idx + 1] = calculate_firing_rate(dt, uu_p[dt_idx], vv_p[dt_idx], wee, wie, wei, wii, tau_e, tau_i)
+    uu_p[dt_idx + 1], vv_p[dt_idx + 1] = calculate_firing_rate(dt, uu_p[dt_idx], vv_p[dt_idx], wee, wie, wei, wii,
+                                                               args.tau_e, args.tau_i, args.sei)
 
 tplot = np.linspace(0, tstop, tstop/dt + 1)
 plt.plot(tplot, uu_p, label='uu', color='red')
 plt.plot(tplot, vv_p, label='vv', color='blue')
+plt.legend()
 plt.xlabel('Time')
+plt.ylabel('Proportion of firing cells')
+plt.savefig('EI_activity.png')
+
 plt.figure()
 plt.plot(tplot, abs(uu_p - vv_p), label='sum', color='green')
+plt.ylabel('Proportion of firing cells')
 plt.xlabel('Time')
 plt.legend()
 plt.show()
