5989edb6f26e2e408ff52a741e337909c465b6ae
Jessica Dafflon <jejedafflon@gmail.com>
2018-07-23 15:35:31 +0100

    Python: Incorporate the plotting of the perideogram on the calculation

--- Python/intralaminar.py
+++ Python/intralaminar.py
@@ -43,8 +43,10 @@ wei = -3.25
 wie = 3.5
 wii = -2.5
 
-dt = 0.2e-3
+dt = 2e-4
 tstop = 25
+transient = 5
+
 t = np.linspace(0, tstop, tstop/dt)
 uu_p = np.zeros((len(t) + 1, 1))
 vv_p = np.zeros((len(t) + 1, 1))
@@ -93,17 +95,36 @@ plt.legend()
 plt.show()
 
 
-y = abs(fft(np.squeeze(uu_p)))
-freqs, P_xx = signal.periodogram(y, 2**8, scaling='density')
-plt.plot(freqs, P_xx)
-plt.xlabel('Frequency')
-plt.ylabel('PSD')
-plt.show()
+#m_file = scmat.loadmat(matfile)
+restate = uu_p
+# discard the first time points
+lower_boundary = int((round(transient + dt) / dt))
+restate = restate[lower_boundary:-1]
 
-# plot semilogaritmic plot
-plt.semilogy(freqs, P_xx)
-plt.xlabel('Frequency')
-plt.ylabel('PSD')
-plt.ylim(10**-5, 10**3)
+
+N = restate.shape[0]
+# sampling frequency to calculate the peridogram
+fs = 1/dt
+# define window to use for the periodogram calculation
+win = signal.get_window('boxcar', N)
+# Calculate fft (number of freq. points at which the psd is estimated)
+# Calculate the max power of 2 and find the maximum value
+pow2 = int(round(math.log(N, 2)))
+fft = max(256, 2 ** pow2)
+
+# perform periodogram on restate
+# fxx, pxx = signal.periodogram(restate, nfft=fft, fs=fs, window=win, return_onesided=True,
+#                               scaling='density', detrend=False)
+fxx2, pxx2 = signal.periodogram(np.transpose(restate), nfft=fft, fs=fs, window=win, return_onesided=True,
+                                scaling='density', detrend=False)
+
+
+# The fancy plot is only for layer 2/3 and it is average over 10 rounds.
+# TODO:smooth data and save it
+# Todo: look at the jupyternotebook and find how to start from 0
+plt.figure()
+plt.semilogy(fxx2, np.squeeze(pxx2))
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('PSD (V**2/Hz)')
 plt.show()
-print('hi')
+print('hello')
