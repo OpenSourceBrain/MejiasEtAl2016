740b24dc066ace15a70ba0b5015cf52d90e928eb
Jessica Dafflon <jejedafflon@gmail.com>
2018-07-23 18:51:18 +0100

    Python: Transform periodogram analysis into a function

--- Python/peridogramtest.py
+++ Python/peridogramtest.py
@@ -6,6 +6,31 @@ import scipy.io.matlab as scmat
 import math
 import numpy as np
 
+
+def down_sampled_periodogram(restate, fft, fs, win, min_freq):
+    # perform periodogram on restate
+    fxx2, pxx2 = signal.periodogram(np.squeeze(restate), nfft=fft, fs=fs, window=win, return_onesided=True,
+                                    scaling='density', detrend=False)
+
+    # Compress the data by sampling every 5 points.
+    bin_size = 5
+    # We start by calculating the number of index needed to in order to sample every 5 points and select only those
+    remaining = fxx2.shape[0] - (fxx2.shape[0] % bin_size)
+    fxx2 = fxx2[0:remaining]
+    pxx2 = pxx2[0:remaining]
+    # Then we calculate the average signal inside the specified bins
+    # Note: the output needs to be an np.array in order to be able to use np.where afterwards
+    pxx_bin = np.asarray([np.mean(pxx2[i:i+bin_size]) for i in range(0, len(pxx2), bin_size)])
+    fxx_bin = np.asarray([np.mean(fxx2[i:i+bin_size]) for i in range(0, len(fxx2), bin_size)])
+
+    # find the frequency of the oscillations (note used at the moment)
+    z = np.where(fxx_bin > min_freq)
+    pxx_freq = pxx_bin[z]
+    fxx_freq = fxx_bin[z]
+
+    return pxx_bin, fxx_bin
+
+
 matfile = '../Matlab/fig2/peridogram.mat'
 dt = 2e-04
 min_freq = 10
@@ -14,7 +39,7 @@ restate = m_file['restate']
 restate = np.transpose(restate)
 
 N = restate.shape[0]
-# sampling frequency to calculate the peridogram
+# sampling frequency to calculate the periodogram
 fs = 1/dt
 # define window to use for the periodogram calculation
 win = signal.get_window('boxcar', N)
@@ -23,25 +48,8 @@ win = signal.get_window('boxcar', N)
 pow2 = int(round(math.log(N, 2)))
 fft = max(256, 2 ** pow2)
 
-# perform periodogram on restate
-fxx2, pxx2 = signal.periodogram(np.squeeze(restate), nfft=fft, fs=fs, window=win, return_onesided=True,
-                              scaling='density', detrend=False)
-
-# Compress the data by sampling every 5 points.
-bin_size = 5
-# We start by calculating the number of index needed to in order to sample every 5 points and select only those
-remaining = fxx2.shape[0] - (fxx2.shape[0] % bin_size)
-fxx2 = fxx2[0:remaining]
-pxx2 = pxx2[0:remaining]
-# Then we calculate the average signal inside the specified bins
-# Note: the output needs to be an np.array in order to be able to use np.where afterwards
-pxx_bin = np.asarray([np.mean(pxx2[i:i+bin_size]) for i in range(0, len(pxx2), bin_size)])
-fxx_bin = np.asarray([np.mean(fxx2[i:i+bin_size]) for i in range(0, len(fxx2), bin_size)])
-
-# find the frequency of the oscillations (note used at the moment)
-z = np.where(fxx_bin > min_freq)
-pxx_freq = pxx_bin[z]
-fxx_freq = fxx_bin[z]
+# Calculate downsampled periodogram
+pxx_bin, fxx_bin = down_sampled_periodogram(restate, fft, fs, win, min_freq)
 
 # Todo: look at the jupyternotebook and find how to start from 0
 plt.figure()
