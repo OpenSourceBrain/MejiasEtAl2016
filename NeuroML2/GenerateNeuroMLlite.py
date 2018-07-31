13823a2cdc193771b7f921c70619ff53fcdd4973
Padraig Gleeson <p.gleeson@gmail.com>
2018-07-31 15:28:42 +0100

    Update to NML version

--- NeuroML2/GenerateNeuroMLlite.py
+++ NeuroML2/GenerateNeuroMLlite.py
@@ -1,7 +1,5 @@
 from neuromllite import Network, Cell, InputSource, Population, Synapse,RectangularRegion,RandomLayout
 from neuromllite import Projection, RandomConnectivity, Input, Simulation
-from neuromllite.NetworkGenerator import generate_and_run
-from neuromllite.NetworkGenerator import generate_neuroml2_from_network
 import sys
 
 ################################################################################
@@ -10,18 +8,18 @@ import sys
 net = Network(id='MejiasFig2')
 net.notes = 'Testing...'
 
-net.parameters = { 'input':5 }
+#net.parameters = { }
 
 l23ecell = Cell(id='L23_E', lems_source_file='Prototypes.xml')
 l23icell = Cell(id='L23_I', lems_source_file='RateBased.xml') #  hack to include this file too.  
-l5ecell = Cell(id='L5_I', lems_source_file='NoisyCurrentSource.xml') #  hack to include this file too.  
-l5icell = Cell(id='L5_I', lems_source_file='Prototypes.xml')
+l56ecell = Cell(id='L56_E', lems_source_file='NoisyCurrentSource.xml') #  hack to include this file too.  
+l56icell = Cell(id='L56_I', lems_source_file='Prototypes.xml')
 
 
 net.cells.append(l23ecell)
 net.cells.append(l23icell)
-net.cells.append(l5ecell)
-net.cells.append(l5icell)
+net.cells.append(l56ecell)
+net.cells.append(l56icell)
 
 
 input_source0 = InputSource(id='iclamp0', 
@@ -30,19 +28,27 @@ input_source0 = InputSource(id='iclamp0',
                            
 net.input_sources.append(input_source0)
 
-l23 = RectangularRegion(id='L23', x=0,y=0,z=0,width=100,height=100,depth=10)
+l23 = RectangularRegion(id='L23', x=0,y=100,z=0,width=100,height=100,depth=10)
 net.regions.append(l23)
 
+l56 = RectangularRegion(id='L56', x=0,y=0,z=0,width=100,height=100,depth=10)
+net.regions.append(l56)
     
-color_str = {'l23e':'.8 0 0','l23i':'0 0 .8'}
+color_str = {'l23e':'.8 0 0','l23i':'0 0 .8',
+             'l56e':'1 .2 0','l56i':'0 .2 1'}
 
-pE = Population(id='L23_E', size=1, component=l23ecell.id, properties={'color':color_str['l23e']},random_layout = RandomLayout(region=l23.id))
-pI = Population(id='L23_I', size=1, component=l23icell.id, properties={'color':color_str['l23i']},random_layout = RandomLayout(region=l23.id))
+pl23e = Population(id='L23_E', size=1, component=l23ecell.id, properties={'color':color_str['l23e']},random_layout = RandomLayout(region=l23.id))
+pl23i = Population(id='L23_I', size=1, component=l23icell.id, properties={'color':color_str['l23i']},random_layout = RandomLayout(region=l23.id))
 
-net.populations.append(pE)
-net.populations.append(pI)
+pl56e = Population(id='L56_E', size=1, component=l56ecell.id, properties={'color':color_str['l56e']},random_layout = RandomLayout(region=l56.id))
+pl56i = Population(id='L56_I', size=1, component=l56icell.id, properties={'color':color_str['l56i']},random_layout = RandomLayout(region=l56.id))
 
-pops = [pE,pI]
+net.populations.append(pl23e)
+net.populations.append(pl23i)
+
+net.populations.append(pl56e)
+net.populations.append(pl56i)
+
 
 
 net.synapses.append(Synapse(id='rs', 
@@ -54,21 +60,28 @@ wie = 3.5; wii = -2.5
 W = [[wee,   wei],
     [wie,   wii]]
     
-for pre in pops:
-    for post in pops:
-        
-        weight = W[pops.index(post)][pops.index(pre)]
-        print('Connection %s -> %s weight %s'%(pre.id, post.id, weight))
-        if weight!=0:
-            
-            net.projections.append(Projection(id='proj_%s_%s'%(pre.id,post.id),
-                                              presynaptic=pre.id, 
-                                              postsynaptic=post.id,
-                                              synapse='rs',
-                                              type='continuousProjection',
-                                              delay=0,
-                                              weight=weight,
-                                              random_connectivity=RandomConnectivity(probability=1)))
+def internal_connections(pops):
+    for pre in pops:
+        for post in pops:
+
+            weight = W[pops.index(post)][pops.index(pre)]
+            print('Connection %s -> %s weight %s'%(pre.id, post.id, weight))
+            if weight!=0:
+
+                net.projections.append(Projection(id='proj_%s_%s'%(pre.id,post.id),
+                                                  presynaptic=pre.id, 
+                                                  postsynaptic=post.id,
+                                                  synapse='rs',
+                                                  type='continuousProjection',
+                                                  delay=0,
+                                                  weight=weight,
+                                                  random_connectivity=RandomConnectivity(probability=1)))
+
+pops = [pl23e,pl23i]
+internal_connections(pops)
+
+pops = [pl56e,pl56i]
+internal_connections(pops)
                                
 '''
 net.inputs.append(Input(id='modulation',
@@ -88,7 +101,6 @@ sim = Simulation(id='Sim%s'%net.id,
                  network=new_file,
                  duration='1000',
                  dt='0.025',
-                 recordTraces={'all':'*'},
                  recordRates={'all':'*'})
                  
 sim.to_json_file()
