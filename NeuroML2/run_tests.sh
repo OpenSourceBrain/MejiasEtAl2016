a78ae601243e6f8e57edacc52ed06399cad87572
Padraig Gleeson <p.gleeson@gmail.com>
2018-09-14 14:17:47 +0100

    Start longer run at 1Hz

--- NeuroML2/run_tests.sh
+++ NeuroML2/run_tests.sh
@@ -10,9 +10,9 @@ python Mejias-2016.py -noise 0 -analysis debug -noconns -testduration 1000 -init
 python Mejias-2016.py -noise 0 -analysis debug -testduration 1000 -initialrate 5
 
 # Run long duration test, with conns
-python Mejias-2016.py -noise 1 -analysis debug -testduration 50000 -initialrate 5
+python Mejias-2016.py -noise 1 -analysis debug -testduration 50000 -initialrate 1
 
 # Run long duration test, no conns
-python Mejias-2016.py -noise 1 -analysis debug -noconns -testduration 50000 -initialrate 5
+python Mejias-2016.py -noise 1 -analysis debug -noconns -testduration 50000 -initialrate 1
 
 cd ../NeuroML2
