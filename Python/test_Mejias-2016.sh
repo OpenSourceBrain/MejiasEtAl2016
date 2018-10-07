8a7de5ff75a07e642ba018444a63fecc6d94bf2f
Jessica Dafflon <jejedafflon@gmail.com>
2018-10-07 12:52:16 +0100

    Update bash test file

--- Python/test_Mejias-2016.sh
+++ Python/test_Mejias-2016.sh
@@ -4,15 +4,32 @@ set -e
 
 # Run one simulation for L2/3 layers when there is no additional external current
 echo "Run simple simulation and save txt file"
-python Mejias-2016.py -noise 1 -analysis debug_neuroML
+python Mejias-2016.py -noise 1 -analysis debug
 
 # Note: the values for tau in the paper represent the values in ms. Here we use
 # s.
 # This will create the Fig2b from the Mejias-2016 paper
-echo "Run intralaminar simulation for L2/3...(it will take a few mins)"
-python Mejias-2016.py -noise 1 -analysis intralaminar
+echo "------------------------------------------------------------------------"
+echo "Run intralaminar simulation for L2/3"
+if [ -f intralaminar/L2_3_simulation.pckl ]; then
+    echo "Remove previous simulation file"
+    rm intralaminar/L2_3_simulation.pckl
+fi
+python Mejias-2016.py -noise 1 -analysis intralaminar -nogui
 
-echo "30 traces of activity in layer 5/6 (For the total time lenght the
-simulation will take ages)"
-python Mejias-2016.py -noise 1 -analysis interlaminar
+echo "------------------------------------------------------------------------"
+echo "Generate Power Spectrum for L2/3 and L5/6"
+if [ -f interlaminar_a/simulation.pckl ]; then
+    echo "Remove previous simulation file"
+    rm interlaminar_a/simulation.pckl
+fi
+python Mejias-2016.py -noise 1 -analysis interlaminar_a -nogui
 
+echo "------------------------------------------------------------------------"
+echo "Generate a set of 30 traces of activity in layer 5/6 (in gray) and their
+average (in blue) L5/6"
+if [ -f interlaminar_b/simulation.pckl ]; then
+    echo "Remove previous simulation file"
+    rm interlaminar_b/simulation.pckl
+fi
+python Mejias-2016.py -noise 1 -analysis interlaminar_b -nogui
