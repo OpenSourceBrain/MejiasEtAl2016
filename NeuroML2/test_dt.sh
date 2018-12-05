52daa044d65cf4ff7bd3ee3f4c5fdc3b13cc8283
Padraig Gleeson <p.gleeson@gmail.com>
2018-12-05 17:17:45 +0000

    Update test names

--- NeuroML2/test_dt.sh
+++ NeuroML2/test_dt.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
 set -e
+
+#####   
+##    This will test to make sure the NeuroML model gives similar results 
+##    independent of the timestep, dt
+##
+##    See also https://github.com/OpenSourceBrain/MejiasEtAl2016/issues/3
+#####
 
 cd ../Python
 
@@ -7,5 +15,6 @@ python Mejias-2016.py -analysis debug -noconns -testduration 50000 -dt 2e-4 -ini
 
 python Mejias-2016.py -analysis debug -noconns -testduration 50000 -dt 2e-5 -initialrate 1
 
-
 cd ../NeuroML2
+
+python GenerateNeuroMLlite.py -dt
