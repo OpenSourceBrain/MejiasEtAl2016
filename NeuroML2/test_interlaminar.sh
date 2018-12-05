5db4bccc0b63c11fd93936e3301a66565c3ec5b1
Padraig Gleeson <p.gleeson@gmail.com>
2018-12-05 17:32:52 +0000

    Add test

---
+++ NeuroML2/test_interlaminar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+#####   
+##    This will test to make sure the interlaminar example matches between
+##    Python and NeuroML implementations
+##
+##    See also https://github.com/OpenSourceBrain/MejiasEtAl2016/pull/13
+#####
+
+cd ../Python
+
+if [ -f interlaminar_a/simulation.pckl ]; then
+    echo "Remove previous simulation file"
+    rm interlaminar_a/simulation.pckl
+fi
+python Mejias-2016.py -analysis interlaminar_a -nogui
+
+cd ../NeuroML2
+
+python GenerateNeuroMLlite.py -interlaminar
